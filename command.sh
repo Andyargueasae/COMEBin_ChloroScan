975c9c7c1d9cccb732e2dff22c3fa4edc861f500
Yuhao Tong <yuhtong@rs-l-16rrt3.desktop.cloud.unimelb.edu.au>
2025-12-05 10:10:08 +1100

    Update gitignore and store a script for running comebin

--- command.sh
+++ command.sh
@@ -1,4 +1,4 @@
-bash bin/run_comebin.sh -a /home/yuhtong/scratch/andy/packed_262genome_8Gbp/contigs/plastid.fasta \
-    -o /home/yuhtong/scratch/andy/packed_262genome_8Gbp/comebin_output \
-    -p /home/yuhtong/scratch/andy/packed_262genome_8Gbp/bam \
-    -b 128 -d /home/yuhtong/scratch/andy/COMEBin_ChloroScan/checkm_db
+bash ./run_comebin.sh -a /data/CAMISIM_2/CAMISIM/maximum_deep_seq/sample_0/plastid.fasta \
+    -o /home/student.unimelb.edu.au/yuhtong/andy/data/COMEBin_ChloroScan/maximum_deepseq \
+    -p /data/CAMISIM_2/CAMISIM/maximum_deep_seq/packed_maximum_deepseq_metag/bam \
+    -b 128 -d /data/COMEBin_ChloroScan/checkm_db -t 12
